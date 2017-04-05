b5fb05d4a6cf07ff714b9530f9f7ee39af4fcabf
Klaudiusz Staniek <kstaniek@gmail.com>
2017-04-05 15:41:46 +0200

    Modified disconnect for TELNETCONSOLE

--- condoor/protocols/telnet.py
+++ condoor/protocols/telnet.py
@@ -150,10 +150,13 @@ class TelnetConsole(Telnet):
         logger.debug("TELNETCONSOLE disconnect")
         try:
             while self.device.mode != 'global':
-                self.device.send('exit')
+                self.device.send('exit', timeout=10)
+        except OSError:
+            logger.debug("TELNETCONSOLE already disconnected")
+        except pexpect.TIMEOUT:
+            logger.debug("ELNETCONSOLE unable to get the root prompt")
 
-            self.device.send('exit', wait_for_string=driver.press_return_re)
-
+        try:
             self.device.ctrl.send(chr(4))
         except OSError:
             logger.debug("TELNETCONSOLE already disconnected")
