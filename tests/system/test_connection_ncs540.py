416326af2edb3a7791bcfab0a68b497d3c211274
Klaudiusz Staniek <kstaniek@gmail.com>
2017-09-14 21:55:51 +0200

    NCS540 and NCS560 platform support added

--- tests/system/test_connection_ncs540.py
+++ tests/system/test_connection_ncs540.py
@@ -1,12 +1,12 @@
 from tests.system.common import CondoorTestCase, StopTelnetSrv, StartTelnetSrv
-from tests.dmock.dmock import NCS5500Handler
+from tests.dmock.dmock import NCS540Handler
 from tests.utils import remove_cache_file
 
 import condoor
 
 
-class TestNCS5500Connection(CondoorTestCase):
-    @StartTelnetSrv(NCS5500Handler, 10023)
+class TestNCS540Connection(CondoorTestCase):
+    @StartTelnetSrv(NCS540Handler, 10023)
     def setUp(self):
         CondoorTestCase.setUp(self)
 
@@ -14,7 +14,7 @@ class TestNCS5500Connection(CondoorTestCase):
     def tearDown(self):
         pass
 
-    def test_NCS5500_1_discovery(self):
+    def test_NCS540_1_discovery(self):
 
         remove_cache_file()
 
@@ -26,22 +26,22 @@ class TestNCS5500Connection(CondoorTestCase):
         self.assertEqual(conn.is_discovered, True, "Not discovered properly")
         self.assertEqual(conn.hostname, "ios", "Wrong Hostname: {}".format(conn.hostname))
         self.assertEqual(conn.family, "NCS5500", "Wrong Family: {}".format(conn.family))
-        self.assertEqual(conn.platform, "NCS-5508", "Wrong Platform: {}".format(conn.platform))
+        self.assertEqual(conn.platform, "N540", "Wrong Platform: {}".format(conn.platform))
         self.assertEqual(conn.os_type, "eXR", "Wrong OS Type: {}".format(conn.os_type))
-        self.assertEqual(conn.os_version, "6.0.1", "Wrong Version: {}".format(conn.os_version))
+        self.assertEqual(conn.os_version, "6.3.2.09I", "Wrong Version: {}".format(conn.os_version))
         self.assertEqual(conn.udi['name'], "Rack 0", "Wrong Name: {}".format(conn.udi['name']))
-        self.assertEqual(conn.udi['description'], "NCS5500 8 Slot Single Chassis",
+        self.assertEqual(conn.udi['description'], "NCS 540 Series 24x1/10GE, 8x10/25GE, 2x100GE Fixed Chassis",
                          "Wrong Description: {}".format(conn.udi['description']))
-        self.assertEqual(conn.udi['pid'], "NCS-5508", "Wrong PID: {}".format(conn.udi['pid']))
+        self.assertEqual(conn.udi['pid'], "N540-24Z8Q2C-M", "Wrong PID: {}".format(conn.udi['pid']))
         self.assertEqual(conn.udi['vid'], "V01", "Wrong VID: {}".format(conn.udi['vid']))
-        self.assertEqual(conn.udi['sn'], "FGE194714QX", "Wrong S/N: {}".format(conn.udi['sn']))
+        self.assertEqual(conn.udi['sn'], "FOC2118R11N", "Wrong S/N: {}".format(conn.udi['sn']))
         self.assertEqual(conn.prompt, "RP/0/RP0/CPU0:ios#", "Wrong Prompt: {}".format(conn.prompt))
         with self.assertRaises(condoor.CommandSyntaxError):
             conn.send("wrongcommand")
 
         conn.disconnect()
 
-    def test_NCS5500_2_connection_wrong_user(self):
+    def test_NCS540_2_connection_wrong_user(self):
         urls = ["telnet://root:admin@127.0.0.1:10023"]
         self.conn = condoor.Connection("host", urls, log_session=self.log_session, log_level=self.log_level)
 
@@ -50,7 +50,7 @@ class TestNCS5500Connection(CondoorTestCase):
 
         self.conn.disconnect()
 
-    def test_NCS5500_3_connection_refused(self):
+    def test_NCS540_3_connection_refused(self):
         urls = ["telnet://admin:admin@127.0.0.1:10024"]
         self.conn = condoor.Connection("host", urls, log_session=self.log_session, log_level=self.log_level)
         with self.assertRaises(condoor.ConnectionError):
