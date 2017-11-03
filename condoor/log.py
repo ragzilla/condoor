0cb7a4097c1401826b165d22048cc0a3f468eb0d
Klaudiusz Staniek <kstaniek@gmail.com>
2017-11-03 01:40:16 +0100

    Fixed PEP8 issue

--- condoor/log.py
+++ condoor/log.py
@@ -8,7 +8,7 @@ def currentframe():
     """Return the frame object for the caller's stack frame."""
     try:
         raise Exception
-    except:
+    except:  # noqa
         return sys.exc_info()[2].tb_frame.f_back
 
 
