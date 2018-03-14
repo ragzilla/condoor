605e824975226dec7c34f386dc71bc00ccc3d990
Klaudiusz Staniek <kstaniek@gmail.com>
2018-03-14 19:58:40 +0100

    test db removal fixed

--- tests/utils.py
+++ tests/utils.py
@@ -1,9 +1,14 @@
 import os
-import condoor
+from condoor.connection import _CACHE_FILE
 
 
 def remove_cache_file():
     try:
-        os.remove('/tmp/condoor.{}.db'.format(condoor.__version__))
+        os.remove(_CACHE_FILE)
+    except OSError:
+        pass
+
+    try:
+        os.remove(_CACHE_FILE + '.db')
     except OSError:
         pass
